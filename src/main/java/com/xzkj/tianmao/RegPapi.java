9161ed10e1056056e2666c93eb42f380281baebe
XiaoZhengRS <2707610100@qq.com>
2019-06-24 13:52:50 +0800

    增减Papi的返回值 之前已经写好了忘记更新了

--- src/main/java/com/xzkj/tianmao/RegPapi.java
+++ src/main/java/com/xzkj/tianmao/RegPapi.java
@@ -1,24 +1,29 @@
 package com.xzkj.tianmao;
 
 import me.clip.placeholderapi.PlaceholderHook;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+
+import java.io.File;
 
 public class RegPapi extends PlaceholderHook {
     @Override
     public String onPlaceholderRequest(Player player, String identifier) {
         TMHashMap hashmap = new TMHashMap();
         Api api = hashmap.getData(player);
+        File PlayerShopFile = new File(MainData.PlayerShop.getAbsolutePath() + "\\" + player.getName() + ".yml");
+        YamlConfiguration playerShopYml = YamlConfiguration.loadConfiguration(PlayerShopFile);
         if (identifier.equalsIgnoreCase("PlayerTianMaoName")){
-            return "";
+            return playerShopYml.getString("商店名称");
         }
         if (identifier.equalsIgnoreCase("PlayerTianMaoNumberOfProducts")){
-            return "";
+            return playerShopYml.getString("商店商品数量");
         }
         if (identifier.equalsIgnoreCase("PlayerTianMaoSalesVolume")){
-            return "";
+            return playerShopYml.getString("商店积分");
         }
         if (identifier.equalsIgnoreCase("PlayerTianMaoLevel")){
-            return "";
+            return api.getTianMaoShopSegment(player);
         }
         return "作者:小正QQ:1419158026";
 
